3eaecb68c146535e8a0d1c9ad555a2ed423ac52a
duquangtrung <duquangtrung2004@gmail.com>
2026-04-20 07:50:57 +0700

    commit lần 3

---
+++ app/src/main/java/thigk2/duquangtrung/DanhNhan.java
@@ -0,0 +1,12 @@
+package thigk2.duquangtrung;
+
+public class DanhNhan {
+    String ten, que;
+    int hinh;
+
+    public DanhNhan(String ten, String que, int hinh) {
+        this.ten = ten;
+        this.que = que;
+        this.hinh = hinh;
+    }
+}
